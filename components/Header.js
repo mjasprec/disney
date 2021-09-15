31455752afc13c2a71ae56ae743569b9fd89d9e0
mjasprec14 <michaeljosephasprec1@gmail.com>
2021-09-15 21:01:25 +0800

    fix: header styles

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,10 @@
 import Image from "next/image";
+import {
+  HomeIcon,
+  SearchIcon,
+  PlusIcon,
+  StarIcon,
+} from "@heroicons/react/solid";
 
 const Header = () => {
   return (
@@ -9,6 +15,38 @@ const Header = () => {
         height={80}
         className="cursor-pointer"
       />
+
+      <div className="hidden ml-10 md:flex items-center space-x-6">
+        <a className="group header-link">
+          <HomeIcon className="h-4" />
+          <span className="span">Home</span>
+        </a>
+
+        <a className="header-link group">
+          <SearchIcon className="h-4" />
+          <span className="span">Search</span>
+        </a>
+
+        <a className="header-link group">
+          <PlusIcon className="h-4" />
+          <span className="span">Watchlist</span>
+        </a>
+
+        <a className="header-link group">
+          <StarIcon className="h-4" />
+          <span className="span">Originals</span>
+        </a>
+
+        <a className="header-link group">
+          <img src="/images/movie-icon.svg" alt="" className="h-5" />
+          <span className="span">Movies</span>
+        </a>
+
+        <a className="header-link group">
+          <img src="/images/series-icon.svg" alt="" className="h-5" />
+          <span className="span">Series</span>
+        </a>
+      </div>
     </div>
   );
 };
